b3d5526877353e130a8b2655d796a388e566b731
Rosfran Lins Borges <rosfran.borges@cnj.jus.br>
2021-10-28 00:00:17 -0300

    Some changes.

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module "github.com/rosfran/interview-accountapi"
+module github.com/rosfran/interview-accountapi
 
-go 1.15
+go 1.17
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"github.com/rosfran/interview-accountapi/models"
 )
 
 type Response struct {
